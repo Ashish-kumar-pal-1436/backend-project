ba7f02cce0d5ddb964bdbb24dbb3e00c0d553143
ASHISH KUMAR PAL <106040189+Ashish-kumar-pal-1436@users.noreply.github.com>
2026-07-25 10:10:08 +0530

    login logout routes and middleware

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -4,6 +4,24 @@ import {User} from '../models/user.model.js'
 import {uploadOnCloudinary} from '../utils/cloudinary.js'
 import { ApiResponce } from '../utils/ApiResponce.js'
 
+const generateAccessAndRefreshToken = async (userId) =>{
+
+    try {
+
+        const user = await User.findById(userId)
+        const accessToken =  user.generateAccessToken()
+        const refreshToken=  user.generateRefreshToken()
+
+        user.refreshToken = refreshToken
+        await user.save({validateBeforeSave: false}) 
+
+        return {accessToken, refreshToken}
+
+    } catch (error) {
+        throw new ApiError(500,  error.message || "Somethingw went wrong ")
+    }
+}
+
 const registerUser = asyncHandler(async (req, res) =>{
      // get user details from frontend
      // validation - not empty
@@ -83,6 +101,89 @@ const registerUser = asyncHandler(async (req, res) =>{
         new ApiResponce(200, createdUser, "User Registered successfully")
     )
 
+}) 
+
+const loginUser = asyncHandler(async (req, res) => {
+      // req body --> data
+      // username or email --> login
+      // find the user
+      // password check
+      // access and refresh token
+      // send cookie
+      // send responce 
+
+    // req body --> getting data from user
+     const {email, password, username} = req.body
+     if(!username && !email){
+        throw new ApiError(400, "username or email required")
+     } 
+
+     //find the user
+     const user = await User.findOne({
+        $or: [{username}, {email}]
+     })
+
+     if(!user){
+        throw new ApiError(404, "User doesn't exist")
+     } 
+     
+     // password check
+     const isPasswordValid =  await user.isPasswordCorrect(password)
+     
+     if(! isPasswordValid){
+        throw new ApiError(401, "Invalid user credentials")
+     } 
+
+    const {accessToken, refreshToken} = await generateAccessAndRefreshToken(user._id)
+    const loggedInUser = await User.findById(user._id).select("-password -refreshToken")
+
+    const options ={
+        httpOnly: true,
+        secure: true,
+    }
+
+    return res.status(200)
+    .cookie("accessToken", accessToken, options)
+    .cookie("refreshToken", refreshToken, options)
+    .json(
+        new ApiResponce(
+            200,
+            {
+                user:loggedInUser, accessToken, refreshToken
+            },
+            "User logged in Successfully"
+        )
+    )
 })
 
-export {registerUser}
+const logoutUser = asyncHandler( async (req, res) => {
+     await User.findByIdAndUpdate(
+        req.user._id,
+        {
+            $set:{
+                refreshToken: undefined
+            }
+        },
+
+        {
+            new: true
+        }
+      )
+
+      const options = {
+        httpOnly: true,
+        secure: true
+      }
+
+      return res
+      .status(200)
+      .clearCookie("accessToken", options)
+      .clearCookie("refreshToken", options)
+      .json(new ApiResponce(201, "User logged Out"))
+})
+
+export {
+    registerUser,
+    loginUser,
+    logoutUser
+}
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,10 +1,12 @@
 import { Router } from "express";
-import { registerUser } from "../controllers/user.controller.js";
+import { loginUser, logoutUser, registerUser } from "../controllers/user.controller.js";
 import {upload} from "../middlewares/multer.middleware.js"
+import { verifyJWT } from "../middlewares/auth.middleware.js";
 
 const router = Router ()
 
 router.route("/register").post(
+
     upload.fields([
         {
             name: "avatar",
@@ -15,6 +17,11 @@ router.route("/register").post(
             maxCount: 1
         }
     ]),
-    registerUser)
+    registerUser) 
+
+router.route("/login").post(loginUser)
+
+//secured routes
+router.route("/logout").post(verifyJWT , logoutUser) 
 
 export default router
